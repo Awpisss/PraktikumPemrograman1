9a1dcebcd0bff42cd220625f55e66dd104262542
Awpisss <mhmmdhafizansari12@gmail.com>
2022-10-05 10:18:41 +0800

    Add files via upload

---
+++ PRAK002-2210817210003-MuhammadHafizAnsari.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+
+int main() {
+    printf("Selamat Pagi, Muhammad Hafiz Ansari \n");
+    printf("Selamat Siang, Muhammad Hafiz Ansari \n");
+    printf("Selamat Malam, Muhammad Hafiz Ansari \n");
+    return 0;
+}
---
+++ PRAK004-2210817210003-MuhammadHafizAnsari.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+int main(){
+    printf("##############################\n");
+    printf("#                            #\n");
+    printf("#   Muhammad Hafiz Ansari    #\n");
+    printf("#      2210817210003         #\n");
+    printf("#                            #\n");
+    printf("##############################\n");
+    return 0;
+}
+
+
+
+
+
+
